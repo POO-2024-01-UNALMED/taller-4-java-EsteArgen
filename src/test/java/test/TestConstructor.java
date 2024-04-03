8a27252770edefc51de3413696d60cb8ca4e9660
EsteArgen <101402836+EsteArgen@users.noreply.github.com>
2024-04-03 14:30:51 +0000

    Comentarios

--- src/test/java/test/TestConstructor.java
+++ src/test/java/test/TestConstructor.java
@@ -4,7 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import classroom.*;
-
+// importanciones
 import java.io.*;
 
 import org.junit.jupiter.api.Test;
